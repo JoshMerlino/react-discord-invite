529af6a2638320c9d543d0962563347d7cbf7438
Josh Merlino <joshua.merlino@gmail.com>
2022-03-23 21:41:07 -0400

    remove jit mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { resolve} from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [ react() ],
-	mode: "jit",
 	build: {
 		lib: {
 			entry: resolve("./src/index.tsx"),
